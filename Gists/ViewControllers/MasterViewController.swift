6251f4dfb5cae4b60d8acde4a87295b90fafa77c
SkyChouqqq <skyc2298@gmail.com>
2019-04-02 18:11:55 +0800

    update masterVC UI

--- Gists/ViewControllers/MasterViewController.swift
+++ Gists/ViewControllers/MasterViewController.swift
@@ -9,13 +9,42 @@
 import UIKit
 
 class MasterViewController:UITableViewController {
+    
+    var detailViewController: DetailViewController? = nil
+    var gists = [Gist]()
+    
+    //MARK:-VC life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addGist))
+        self.navigationItem.rightBarButtonItem = addButton
+        if let split = self.splitViewController {
+            let controllers = split.viewControllers
+            self.detailViewController = (controllers[controllers.count-1] as!
+                UINavigationController).topViewController as? DetailViewController
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         GitHubAPIManager.shared.printPublicGists()
+    }
+    
+    //MARK:- target function
+    @objc func addGist() {
+        
+    }
+    @objc func editGist() {
+        
+    }
+    
+    //MARK:- UI
+    fileprivate func editButtonItem()->UIBarButtonItem {
+        return UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editGist))
     }
 }
